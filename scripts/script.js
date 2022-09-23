a836d9d38bcfb67a67f21a06ca6c3354280228ec
Alex Strickland <alexstrickland@Alexs-MacBook-Pro.local>
2022-09-22 19:33:03 -0500

    cleaned code

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,8 @@
 $(document).ready(function(){
-    // click on an image to get a message 
-    // $("#NameGame").click(function(){
-    //    $("#outputDiv").hide();
-    //   console.log("You clicked ElonDoge. This is a nice Doge, but not the right doge.")
-    //   $("#outputMsg").text("You Lose, Elon Doge is not correct");  $("#outputDiv").toggle();
-   
       $("#NameGame").click(function() {
         onclick=location.href='https://astricklandd.github.io/delta-lab/';
       })
 
-  
       $("#DogeGame").click(function() {
         onclick=location.href='https://astricklandd.github.io/cis376-charlie-lab-1/';
       })
